3fd788c44d3eaa6415c05132b1a739d5d9def716
wjdwlsdnr109 <48301651+wjdwlsdnr109@users.noreply.github.com>
2019-03-07 10:38:34 +0900

    C와 유사한 java 문법 맛보기
    맞다. for문과 While문도 체험해보게 하는 걸 깜빡했다.

---
+++ 0307/src/review_C/EscapeChar.java
@@ -0,0 +1,10 @@
+package review_C;
+
+public class EscapeChar {
+	public static void main(String[] args) {
+		System.out.println("AB" +'\b' + 'c');
+		System.out.println("AB" +'\t' + 'c');
+		System.out.println("AB" + '\n' + 'c');
+		System.out.println("AB" +'\r' + 'c');
+	}
+}
---
+++ 0307/src/review_C/ifelse.java
@@ -0,0 +1,13 @@
+package review_C;
+
+public class ifelse {
+	public static void main(String[] args) {
+		int x = 5;
+		
+		if (x>=5) {
+			System.out.println("x는 5이상입니다.");
+		} else {
+			System.out.println("x는 5미만입니다.");
+		}
+	}
+}
